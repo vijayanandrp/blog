b36aeb04ae5e4d38318c565e0c9dca8afd0cae55
vijayanandrp <vijayanandrp@gmail.com>
2021-07-21 18:05:01 +0000

    New code

--- spark/convert_json_struct_arrary_to_string_spark.py
+++ spark/convert_json_struct_arrary_to_string_spark.py
@@ -21,18 +21,14 @@ df.show()
 
 _df = df
 print(_df.dtypes)
-
 non_struct_cols = [c[0] for c in _df.dtypes if c[1][:6] != 'struct']
 print("non_struct_cols - ", non_struct_cols)
-
 struct_cols = [c[0] for c in _df.dtypes if c[1][:6] == 'struct']
 print('struct_cols - ', struct_cols)
-
 parsed_cols = []
-
 for _col in struct_cols:
     string_list = []
-    arrary_list = []
+    array_list = []
     print(f'[*] {_col}')
     flat_cols = list(_df.select(_col + '.*').columns)
     for _key in flat_cols:
@@ -40,14 +36,29 @@ for _col in struct_cols:
         if _df.select(f'{_col}.{_key}').dtypes[0][1][:6] in ['string']:
             string_list.append(f'{_col}.{_key}')
         elif _df.select(f'{_col}.{_key}').dtypes[0][1][:5] in ['array']:
-            arrary_list.append(f'{_col}.{_key}')
-    if not arrary_list and string_list:
+            for x in df.select(explode(f'{_col}.{_key}').alias('tmp')).select('tmp.*').columns:
+                array_list.append(f'{_col}.{_key}.{x}')
+    if not array_list and string_list:
         _df = _df.withColumn(_col, concat_ws('-', *string_list))
         parsed_cols.append(_col)
+    elif array_list and not string_list:
+        if len(array_list) == 1:
+            _df = _df.withColumn(_col, concat_ws('-', *array_list))
+            parsed_cols.append(_col)
+    elif array_list and string_list:
+        if len(array_list) == 1:
+            _df = _df.withColumn(_col+'_tmp', concat_ws('-', *array_list))
+            string_list.append(_col+'_tmp')
+            _df = _df.withColumn(_col, concat_ws('-', *string_list))
+            parsed_cols.append(_col)
+
 
 _df.select(non_struct_cols + parsed_cols).show(10, False)
 
 
+    elif arrary_list and string_list:
+        _df = _df.withColumn(_col, concat_ws('-', *string_list)).withColumn("tmp", arrays_zip(*arrary_list)).withColumn("tmp", explode("tmp")).select([col("tmp."+c).alias(c) for c in arrary_list])
+        parsed_cols.append(_col)
 flat_df = _df.select(flat_cols +
                            [col(nc + '.' + c).alias(nc + '_' + c)
                             for nc in nested_cols
