ea07966c54b47b2d09bcfac310d3aa3ea6a1ea62
VijayAnandRP <vijayanandrp@gmail.com>
2021-10-16 08:20:03 +0530

    Windows files

---
+++ spark/pyspark/dataframe_class.py
@@ -0,0 +1,30 @@
+import pyspark
+
+
+class SampleDf:
+    def __init__(self):
+        pass
+
+    def pre_validation(self):
+        def check_if_table_exists():
+            pass
+
+        def check_dataframe_count():
+            pass
+
+        def check_datatype():
+            pass
+
+        pass
+
+    def pre_process(self):
+        pass
+
+    def execute(self):
+        pass
+
+    def post_process(self):
+        pass
+
+    def post_validation(self):
+        pass
