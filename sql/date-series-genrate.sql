07e97acdbf06b34f7a4231d4e9a56ddaf7bee28d
VijayAnandRP <vijayanandrp@gmail.com>
2022-01-29 14:50:04 +0530

    Sql

--- sql/date-series-genrate.sql
+++ sql/date-series-genrate.sql
@@ -7,7 +7,7 @@ select *, lead(s.day1) over(order by day1) as next_day from
 
 select to_char(to_date('2022-01-26', 'YYYY-MM-DD') + interval '15 days', 'YYYY-MM-DD')::date 
 
-select to_char('2022-01-26'::date + interval '15 days', 'YYYY-MM-DD')::date 
+select to_char('2022-01-26'::date + '15 days'::interval, 'YYYY-MM-DD')::date 
 
 select date::date from generate_series('2022-01-24'::date, '2022-07-26'::date, '15 days'::interval) date
 /*
