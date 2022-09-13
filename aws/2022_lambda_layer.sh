fc58cc48838db848df71b37ab6687ee0c9baf3d1
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-13 17:16:00 +0100

    Update 2022_lambda_layer.sh

--- aws/2022_lambda_layer.sh
+++ aws/2022_lambda_layer.sh
@@ -4,4 +4,4 @@ python3.8 get -pip.py --user
 mkdir python
 python3.8 -m pip install pandas numpy -t python/
 zip -r layer.zip python
-aws lambda publish-layer-version --layer-name pandas-layer --zip-file fileb://layer.zip --compatible-runtimes python3.8 python3.9 --region eu-west-1
+aws lambda publish-layer-version --layer-name pandas-layer --zip-file fileb://layer.zip --compatible-runtimes python3.8 --region eu-west-1
