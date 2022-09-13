0dca803877627d298dc6a3540e208c9d624fe097
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-13 18:19:36 +0100

    Update 2022_lambda_layer.sh

--- aws/2022_lambda_layer.sh
+++ aws/2022_lambda_layer.sh
@@ -1,6 +1,6 @@
 sudo amazon-linux-extras install python3.8
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3.8 get -pip.py --user
+python3.8 get-pip.py --user
 mkdir python
 python3.8 -m pip install pandas numpy -t python/
 zip -r layer.zip python
