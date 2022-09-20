9d6b1d4a6f256dcd61c973f36c462b3635b15952
Vijay Anand Pandian <vijayanandrp@gmail.com>
2022-09-20 12:35:34 +0100

    Update 2022_lambda_layer.sh

--- aws/2022_lambda_layer.sh
+++ aws/2022_lambda_layer.sh
@@ -18,13 +18,13 @@ python3.8 get-pip.py --user
 
 mkdir python
 
-python3.8 -m pip install pandas numpy psycopg2 SQLAlchemy -t python/
+python3.8 -m pip install --platform=manylinux1_x86_64 --only-binary=:all: pandas numpy psycopg2-binary SQLAlchemy -t python/
 
 zip -r layer.zip python
 
 aws lambda publish-layer-version \
- --layer-name mics-layer \
- --description "Pandas Numpy psycopg2-binary SQLAlchemy" \
- --zip-file fileb://layer.zip \
- --compatible-runtimes python3.8 python3.9 \
- --region eu-west-1
+--layer-name mics-layer \
+--description "Pandas Numpy psycopg2-binary SQLAlchemy" \
+--zip-file fileb://layer.zip \
+--compatible-runtimes python3.8 python3.9 \
+--region eu-west-1
